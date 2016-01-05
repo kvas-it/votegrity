e2780d8dbc76fc39800cf92e1525855556dbf024
Vasily Kuznetsov <kvas.it@gmail.com>
2016-01-05 00:14:48 +0000

    Add synchronisation and security to the store exposed by the server #8.

--- server/routes.js
+++ server/routes.js
@@ -7,16 +7,25 @@
 var express = require('express');
 
 var Store = require('./store');
+var StoreAC = require('./store-access-control');
+var StoreSync = require('./store-sync');
 var storeService = require('./store-service');
+
+function attachStore(app) {
+    var store = new Store(app.get('storeRoot'));
+    var storeAC = new StoreAC(store);
+    var storeSync = new StoreSync(storeAC);
+    app.use('/api/store', storeService(storeSync));
+}
 
 function install(app) {
     app.use('/js', express.static(app.get('clientRoot') + '/js'));
     app.use('/cr', express.static(app.get('componentsRoot') + '/cryptico'));
     app.use('/jq', express.static(app.get('componentsRoot') + '/jquery/dist'));
-    app.use('/api/store', storeService(new Store(app.get('storeRoot'))));
     app.route('/*').get(function (req, res) {
         res.sendFile(app.get('clientRoot') + '/index.html');
     });
+    attachStore(app);
 }
 
 module.exports = {
